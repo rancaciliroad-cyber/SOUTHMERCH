f36e790cac7977550452e49e97ea064eceee04c0
rancaciliroad-cyber <rancaciliroad@gmail.com>
2026-04-15 02:06:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ document.getElementById('cartBtn').onclick = () => {
   document.getElementById('cartModal').style.display = 'block';
 };
 
+// Notifikasi modal
+document.getElementById('notifBtn').onclick = () => {
+  document.getElementById('notifModal').style.display = 'block';
+};
+
 // Tambah ke keranjang
 document.querySelector('.addCart').onclick = () => {
   alert('Produk ditambah ke keranjang!');
 };
+  
