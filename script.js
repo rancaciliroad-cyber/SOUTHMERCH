c09530ff81bb2483fd7d4f5805950d78609deb1b
rancaciliroad-cyber <rancaciliroad@gmail.com>
2026-04-15 01:57:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function tambahKeranjang(nama, harga) {
-  let keranjang = JSON.parse(localStorage.getItem("keranjang")) || [];
-  keranjang.push({ nama, harga });
-  localStorage.setItem("keranjang", JSON.stringify(keranjang));
-  alert(nama + " ditambahkan ke keranjang!");
-}
+// Login modal
+document.getElementById('loginBtn').onclick = () => {
+  document.getElementById('loginModal').style.display = 'block';
+};
+
+// Keranjang modal
+document.getElementById('cartBtn').onclick = () => {
+  document.getElementById('cartModal').style.display = 'block';
+};
+
+// Tambah ke keranjang
+document.querySelector('.addCart').onclick = () => {
+  alert('Produk ditambah ke keranjang!');
+};
